501ff5b7cb3ba1ab392268e80459c5955ecaa107
Sava <savamedvedev@gmail.com>
2023-07-20 15:09:33 +0800

    ci for deployer in yr

---
+++ index.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func main() {
+
+    portStr := os.Getenv("PORT")
+    http.Handle("/deploy", hwHandler{})
+    http.ListenAndServe(":"+portStr, nil)
+
+}
+
+type hwHandler struct{}
+
+func (hwHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+    // 1 Get User and Port From enviroments vars
+
+    sshUser := os.Getenv("SSH_USER")
+    sshHost := os.Getenv("SSH_HOST")
+    // 2 Run as ssh using user and host
+
+    cmd := exec.Command("./deploy", sshUser, sshHost)
+    // 3 Process Error
+    
+    _, err := cmd.StdoutPipe()
+
+    err = cmd.Start()
+
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Error starting cmd", err)
+        return
+    }
+
+    err := cmd.Wait()
+
+    f err != nil {
+        fmt.Fprintln(os.Stderr, "Error waiting for cmd", err)
+        return
+    }
+
+    // 4 Return 200 is success
+
+    writer.WriteHeader(200)
+}
